4b7e2a01678f2733de5e484c1528073f399a6360
Shafalil Gupta <sgupta@stuy.edu>
2018-04-18 09:36:37 -0400

    abstract

---
+++ Deque.java
@@ -0,0 +1,8 @@
+public interface Deque<Card>{
+  public <Card> addFirst();
+  public <Card> removeFirst();
+  public <Card> getFirst();
+  public <Card> addLast();
+  public <Card> removeLast();
+  public <Card> getLast();
+}
